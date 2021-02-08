8b55203137687202263da6b07b1f8f19ddd76914
Vitor Furini <vitorfurini@hotmail.com>
2021-02-07 22:11:40 -0300

    first commit

---
+++ src/main/java/com/dbc/vitorfurini/assembleiadbc/AssembleiaDbcApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.vitorfurini.assembleiadbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaDbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaDbcApplication.class, args);
+	}
+
+}
