c959cbc0bc266a858360d860c020d7505e492b8b
Vitor Furini <vitorfurini@hotmail.com>
2021-02-09 20:31:12 -0300

    inclusçao do arquivo JsonException

--- src/main/java/com/dbc/vitorfurini/assembleiadbc/utils/exception/JsonException.java
+++ src/main/java/com/dbc/vitorfurini/assembleiadbc/utils/exception/JsonException.java
@@ -1,2 +1,22 @@
-package com.dbc.vitorfurini.assembleiadbc.utils.exception;public class JsonException {
+package com.dbc.vitorfurini.assembleiadbc.utils.exception;
+
+public class JsonException extends RuntimeException {
+    private static final long serialVersionUID = -4398891155526530222L;
+
+    public JsonException() {
+        super();
+    }
+
+    public JsonException(Throwable cause) {
+        super(cause);
+    }
+
+    public JsonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public JsonException(String message) {
+        super(message);
+    }
+
 }
